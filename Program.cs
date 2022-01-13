a2889b9498eecbd877e7169089509fb098c2f391
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-01-13 14:42:28 +0530

    Adding Hello World Program

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using System;
+        
+        namespace MyFirstApp
+        {
+            public class Program
+            {
+                public static void Main(string[] args)
+                {
+                    Console.WriteLine("Hello World");
+                    Console.ReadLine();
+                }
+        
+
+            }
+            }
