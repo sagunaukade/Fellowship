e475be82614b7dcca165113e0872b7dcf476dcae
sagunaukade96 <saguuka1996@gmail.com>
2022-01-15 22:31:38 +0530

    Adding Basic Programs

---
+++ Addition.cs
@@ -0,0 +1,18 @@
+using System;
+namespace Fellowship
+
+{
+	class Addition
+	
+	{
+		public static void main(String[] args)
+		
+		{
+			int a = 10;
+			int b = 20;
+			int sum;
+			sum = a + b;
+            Console.WriteLine(sum);
+        }
+	}
+}
