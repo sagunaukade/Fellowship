56c47c7410a6bfb5a1142ae3c739931761516d0f
sagunaukade96 <saguuka1996@gmail.com>
2022-01-16 15:42:42 +0530

    Adding String name programs

---
+++ StringName.cs
@@ -0,0 +1,18 @@
+using System;
+namespace Fellowship
+
+{
+	class StringName
+	
+	{
+		public static void main(String[] args)
+		
+		{
+            string testString;
+			Console.Write("Enter a string - ");
+			testString = Console.ReadLine();
+			Console.WriteLine("You entered '{0}'", testString);
+		}
+	}
+		}
+	
