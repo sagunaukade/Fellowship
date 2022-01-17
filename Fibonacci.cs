4f6c4644d4cb7431ed5be0c91733946036ee50c4
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-01-17 12:06:51 +0530

    Update Fibonacci.cs

--- Fibonacci.cs
+++ Fibonacci.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Demoproject
+namespace Fellowship
 {
-     class Fibonocciseries
+     class Fibonacci
     {
         public void FindFibonocciSeries()
         {
