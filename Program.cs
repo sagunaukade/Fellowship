bbe7a4d29ae304a70210fa412164ff58fe2ebe3e
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-01-17 12:13:01 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,15 +1,18 @@
 using System;
-        
-        namespace MyFirstApp
-        {
-            public class Program
-            {
-                public static void Main(string[] args)
-                {
-                    Console.WriteLine("Hello World");
-                    Console.ReadLine();
-                }
-        
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-            }
-            }
+namespace Fellowship
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome");
+            Console.WriteLine("here comes the change");
+
+        }
+    }
+}
