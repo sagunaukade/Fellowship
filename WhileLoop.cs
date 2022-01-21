708ceef77134cb86847713929d6bfdce2f5215fc
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-01-21 23:29:19 +0530

    Adding while loop program

---
+++ WhileLoop.cs
@@ -0,0 +1,22 @@
+using Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Default
+{
+    public class WhileLoop
+    {
+        public static void Main()
+        {
+            int i = 1;
+            while (i <= 10)
+
+            {
+                Console.WriteLine(i + "");
+                i++;
+            }
+        }
+    }
+}
