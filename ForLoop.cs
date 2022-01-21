fea63c18f751af2716555ce1f04f54233a99de53
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-01-21 23:27:48 +0530

    Adding For loop program

---
+++ ForLoop.cs
@@ -0,0 +1,22 @@
+using Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Default
+{
+	public class ForLoop
+	{
+		public static void main()
+		
+		{
+			for (int i = 0; i <= 10;i++)
+			
+			{
+                Console.WriteLine(i+"");
+            }
+
+		}
+	}
+}
