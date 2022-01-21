bc5e41961c0f7af27e628f23ac52c64f9d86e19b
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-01-21 23:32:04 +0530

    Adding if statement program

---
+++ IfStmt.cs
@@ -0,0 +1,24 @@
+using Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Default
+{
+	public class IfStmt
+	{
+		public static void Main(String[] args)
+		
+		{
+			int PIN = 9112;
+			Console.WriteLine("Saguna.....");
+			if(PIN==9112)
+			
+			{
+				Console.WriteLine("NDA Road.....");
+			}
+            Console.WriteLine("9112356584");
+        }
+	}
+}
