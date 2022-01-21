62c2acb289382cd4e111433b9ec5cb9e1e567d6b
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-01-21 23:28:40 +0530

    Adding do while loop program

---
+++ Dowhile.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Default
+{
+	public class Dowhile
+	{
+		 public static void Main()
+		{
+			int i = 1;
+			do
+			{
+				Console.WriteLine(i + "");
+				i++;
+			}while(i<10)
+	}
+}
