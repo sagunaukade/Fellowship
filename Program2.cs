58bc0a495687435941cedab496e09fb4de530a03
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-01-21 23:33:22 +0530

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Default
+{
+	public class Program2
+	{
+		public static void Main(String[] args)
+		{
+			int num;
+			Console.WriteLine("Enter number");
+			num=Convert.ToInt32(Console.ReadLine());
+            Console.Write(Num);
+
+
+        }
+	}
+}
