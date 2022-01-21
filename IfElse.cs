968ba6f792956f9361c7f1f1d09dfe8c7573bc44
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-01-21 23:31:18 +0530

    Adding if else program

---
+++ IfElse.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Default
+{
+	public class IfElse
+	{
+		public static void main(String[] args)
+		
+		{
+			int age = 38;
+			if(age<=18)
+			
+			{
+				Console.WriteLine("Eligible for vote");
+			}
+			else
+			
+			{
+                Console.WriteLine("Not eligible for vote");
+            }
+		}
+	}
+}
