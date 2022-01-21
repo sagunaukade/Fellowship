4925b468f0ef2a057c5817e5d3d26473f825b2f8
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-01-21 23:33:03 +0530

    Add files via upload

---
+++ Addition.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Default
+{
+	public class Addition
+	{
+		public static void Main(String[] args)
+		
+		{
+			int a = 10, b = 20, r;
+			r = a + b;
+            Console.WriteLine("Addition of two nums" + r);
+        }
+	}
+}
