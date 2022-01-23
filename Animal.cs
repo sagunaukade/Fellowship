65f8ef5f446f790c197156e4ba88380a820d9868
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-01-23 23:55:03 +0530

    Adding method overriding program

---
+++ Animal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    internal class Animal
+    {
+        public virtual void eat()
+        {
+            Console.WriteLine("Eating...");
+        }
+    }
+    public class Dog : Animal
+    {
+        public override void eat()
+        {
+            Console.WriteLine("Eating bread...");
+        }
+    }
+    public class TestOverriding
+    {
+        public static void Main()
+        {
+            Dog d = new Dog();
+            d.eat();
+        }
+    }
+}
+
